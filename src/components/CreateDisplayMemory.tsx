a3b38cc72b884b3c24b4998963c66792c795793a
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:48:13 +0300

    Styled HTML form tag with Tailwind class padding

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -12,8 +12,8 @@ export const CreateDisplayMemory = () => {
                 <p className="text-xl font-medium mt-6">
                     Anytime something positive happens, make a note of it and come back to it later
                 </p>
-                <form  className="">
-                    
+                <form  className="p-2">
+
                 </form>
 
             </section>
