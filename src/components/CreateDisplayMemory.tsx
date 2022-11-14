80b37e9e50346fcae4e6ed1fbd306aef1d841dd3
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:28:41 +0300

    Styled HTML man tag with Tailwind class flex-row on desktop display

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -1,7 +1,7 @@
 
 export const CreateDisplayMemory = () => {
     return (
-        <main className="w-full h-auto flex flex-col ">
+        <main className="w-full h-auto flex flex-col lg:flex-row ">
 
         </main>
     )
