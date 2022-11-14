ebc3dbaf768015a35459b9922cc94c64445486ea
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:38:35 +0300

    Defined HTML h1 tag string value

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -4,7 +4,7 @@ export const CreateDisplayMemory = () => {
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
                 <h1 className="">
-                    
+                    World of the day
                 </h1>
 
             </section>
