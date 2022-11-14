61c12b1318774e6401418e5b2ce35676b243656c
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:37:37 +0300

    Added HTML heading one tag

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -3,6 +3,9 @@ export const CreateDisplayMemory = () => {
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
+                <h1 className="">
+                    
+                </h1>
 
             </section>
 
