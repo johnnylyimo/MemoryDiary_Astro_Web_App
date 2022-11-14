9dd85f69f6682577ae7e6c88c05a3ec95f8de655
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:39:49 +0300

    Styled HTML h1 tag with Tailwind class uppercase

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -3,7 +3,7 @@ export const CreateDisplayMemory = () => {
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
-                <h1 className="text-2xl font-bold ">
+                <h1 className="text-2xl font-bold uppercase ">
                     World of the day
                 </h1>
 
