1175a35afd093925c227e563ab8b6a3163a557ce
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:34:37 +0300

    Styled HTML section tag with Tailwind class max width on desktop display

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -2,7 +2,7 @@
 export const CreateDisplayMemory = () => {
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
-            <section className="max-w-screen-sm mx-auto ">
+            <section className="max-w-screen-sm mx-auto lg:max-w-xxl ">
 
             </section>
 
