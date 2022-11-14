916be4940bacd9adc2baaa1ef6ed384a36622b38
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:31:52 +0300

    Added HTML section tag

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -2,6 +2,9 @@
 export const CreateDisplayMemory = () => {
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
+            <section>
+                
+            </section>
 
         </main>
     )
