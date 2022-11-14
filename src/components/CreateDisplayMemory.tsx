2e929ac57fda10434051766ba6a2a28a540f1b05
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:36:50 +0300

    Styled HTML section tag with Tailwind class bg background

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -2,7 +2,7 @@
 export const CreateDisplayMemory = () => {
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
-            <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 ">
+            <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
 
             </section>
 
