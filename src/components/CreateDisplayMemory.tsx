f2d0f768dcd67c12e7f5ca3865c30542b2674d82
johnnylyimo <johnnylyimo@gmail.com>
2022-11-14 11:30:13 +0300

    Styled HTML main tag with Tailwind class padding

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -1,7 +1,7 @@
 
 export const CreateDisplayMemory = () => {
     return (
-        <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center">
+        <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
 
         </main>
     )
