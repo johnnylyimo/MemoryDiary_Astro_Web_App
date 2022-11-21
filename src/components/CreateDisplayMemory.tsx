d74e4fdb5242831a758d35a644e6ca25544c0b13
johnnylyimo <johnnylyimo@gmail.com>
2022-11-21 11:25:40 +0300

    Defined new editMemoryPopUp useState

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -6,6 +6,8 @@ export const CreateDisplayMemory = () => {
         title: "",
         memory: ""
       });
+
+      const [editMemoryPopUp, setEditMemoryPopUp]=useState(false);
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
