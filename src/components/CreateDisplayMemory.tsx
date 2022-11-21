fb5b19039c5021e2bbbbf2f64a3f01a9d35495c2
johnnylyimo <johnnylyimo@gmail.com>
2022-11-21 12:43:10 +0300

    Imported MdEdit icon from react-icons

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import {MdNotes, } from 'react-icons/md'
+import {MdNotes, MdEdit} from 'react-icons/md'
 
 export const CreateDisplayMemory = () => {
     const memories = [
@@ -52,9 +52,7 @@ export const CreateDisplayMemory = () => {
                             setEditMemoryPopUp(true)
                             setInputedMemory({id:id, memory: memory,title:''})
                         }}>
-                        {/* <Icon name="sort-variant" height="24px"  width="24px" />  */}
-                        <p>{memory}</p>
-                        {/* <Icon name="mdi:pencil" height="24px"  width="24px" /> */}
+                   
                         </div>
                     ))
                 }
