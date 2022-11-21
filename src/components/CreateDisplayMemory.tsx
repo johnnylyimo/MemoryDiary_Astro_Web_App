787a0817c10635fda8d799742068c1dfb0ebdb44
johnnylyimo <johnnylyimo@gmail.com>
2022-11-21 11:26:00 +0300

    Defined new isEmptyEditedMemory useState

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -8,6 +8,8 @@ export const CreateDisplayMemory = () => {
       });
 
       const [editMemoryPopUp, setEditMemoryPopUp]=useState(false);
+      const [isEmptyEditedMemory, setIsEmptyEditedMemory] = useState(false)
+
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
