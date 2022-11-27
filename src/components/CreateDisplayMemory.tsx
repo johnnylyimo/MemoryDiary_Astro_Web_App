d4d0926faad712f8029f173cafe90cc9499b6e8d
johnnylyimo <johnnylyimo@gmail.com>
2022-11-27 14:32:51 +0300

    Styled div tag with Tailwind CSS class translate -y

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -61,7 +61,7 @@ export const CreateDisplayMemory = () => {
 
             {/* Edit Popup */}
             {
-            editMemoryPopUp ? <div className="absolute top-[35%] left-[50%] translate-x-[-50%]"></div> : null
+            editMemoryPopUp ? <div className="absolute top-[35%] left-[50%] translate-x-[-50%] translate-y-[-50%] "></div> : null
             }
             </section>
 
