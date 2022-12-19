181ee32357bb695681b8b1ca291e6cd87e04ae43
johnnylyimo <johnnylyimo@gmail.com>
2022-12-19 11:10:06 +0300

    Initialized try catch block

--- src/pages/api/read.ts
+++ src/pages/api/read.ts
@@ -2,4 +2,9 @@ import { Memory } from "@prisma/client";
 import { prisma } from "../../../utils/prisma";
 export async function get({ params, request }) {
   let memories: Memory[] = [];
+  try {
+    
+  } catch (error) {
+    
+  }
 }
