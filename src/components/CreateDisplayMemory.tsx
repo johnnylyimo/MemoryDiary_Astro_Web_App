0fa22a08063c57dd9ec9d9ed3245e77c0a4818c9
johnnylyimo <johnnylyimo@gmail.com>
2022-12-19 12:24:20 +0300

    Created onSubmitMemory method to handle submit new memory

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -14,6 +14,9 @@ export const CreateDisplayMemory = () => {
       const [editMemoryPopUp, setEditMemoryPopUp]=useState(false);
       const [isEmptyEditedMemory, setIsEmptyEditedMemory] = useState(false)
 
+    // method to handle on adding new memory
+    const onSubmitMemory:FormEventHandler<HTMLFormElement> = async (e) =>{  }
+
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
