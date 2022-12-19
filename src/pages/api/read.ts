0fc8c91a3329b5920d2141093aa808404a7e920a
johnnylyimo <johnnylyimo@gmail.com>
2022-12-19 11:12:58 +0300

    Selected field id to show on querying memory table

--- src/pages/api/read.ts
+++ src/pages/api/read.ts
@@ -3,6 +3,10 @@ import { prisma } from "../../../utils/prisma";
 export async function get({ params, request }) {
   let memories: Memory[] = [];
   try {
-    memories = await prisma.memory.findMany();
+    memories = await prisma.memory.findMany({
+      select: {
+        id: true,
+      },
+    });
   } catch (error) {}
 }
