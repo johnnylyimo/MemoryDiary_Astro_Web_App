b74c3be857d53dd3a1e39979f85871fbf2cfe707
johnnylyimo <johnnylyimo@gmail.com>
2023-01-01 13:14:32 +0300

    Initialized useEffect method

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -26,7 +26,10 @@ export const CreateDisplayMemory = () => {
         })
     }
 
+    useEffect(() =>{
 
+    },[])
+    
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
