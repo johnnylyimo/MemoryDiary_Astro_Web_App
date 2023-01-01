97712c0f5a4022fc35acfa0ac71d0e162bc58ca3
johnnylyimo <johnnylyimo@gmail.com>
2023-01-01 13:15:00 +0300

    Invoked fetchMemories method

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -27,9 +27,9 @@ export const CreateDisplayMemory = () => {
     }
 
     useEffect(() =>{
-
+        fetchMemories()
     },[])
-    
+
     return (
         <main className="w-full h-auto flex flex-col lg:flex-row gap-8 text-center p-4">
             <section className="max-w-screen-sm mx-auto lg:max-w-xxl rounded-lg p-2 bg-[#ce93d8]">
