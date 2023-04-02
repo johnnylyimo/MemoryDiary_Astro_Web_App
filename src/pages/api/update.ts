cbd50811bbc5a948423d02d8a5588ac6d570cc0d
johnnylyimo <johnnylyimo@gmail.com>
2023-04-03 01:49:21 +0300

    Intialized try catch block

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -6,4 +6,10 @@ import { prisma } from "../../../utils/prisma";
 export const put: APIRoute = async ({ request }) => {
     const { id, content } = await request.json();
     let memoryToBeUpdated: Memory;
+
+    try {
+        
+    } catch (error) {
+        
+    }
 }
