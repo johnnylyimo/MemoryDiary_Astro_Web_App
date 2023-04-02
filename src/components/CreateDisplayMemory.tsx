6d5687c68a7a7fee17a7aa5d69698c793cc6372c
johnnylyimo <johnnylyimo@gmail.com>
2023-04-03 02:45:50 +0300

    Defined fetch's header attribute

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -56,6 +56,9 @@ export const CreateDisplayMemory = () => {
         if (inputedMemory.id !== ""){
             await fetch('/api/delete', {
                 method: "DELETE", 
+                headers: {
+                    "Content-Type": "application/json",
+                }, 
             })
         }
     }
