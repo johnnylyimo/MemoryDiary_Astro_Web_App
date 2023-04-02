8d52d2314e9ff20f622e2754c64ca08d8918bf93
johnnylyimo <johnnylyimo@gmail.com>
2023-04-03 01:46:55 +0300

    Defined new put method from Astro APIRoute

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -1,3 +1,8 @@
 import type { APIRoute } from "astro";
 import { Memory } from "@prisma/client";
 import { prisma } from "../../../utils/prisma";
+
+
+export const put: APIRoute = async ({ request }) => {
+    
+}
