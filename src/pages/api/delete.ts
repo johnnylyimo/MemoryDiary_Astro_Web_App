b5fa57e29e79776ea19c51695b04ef37fee12bcc
johnnylyimo <johnnylyimo@gmail.com>
2023-04-03 02:57:25 +0300

    Defined Response's status attribute

--- src/pages/api/delete.ts
+++ src/pages/api/delete.ts
@@ -9,6 +9,7 @@ export const del: APIRoute = async ({ request }) => {
         });
 
         return new Response(JSON.stringify({DeletedSuccessfully:'Memory has been Deleted successfuly'}),{
+            status: 200,
         })
 
     } catch (error) {
