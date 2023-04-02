8839a7cd807615bcc7157f61a5151d58286f3021
johnnylyimo <johnnylyimo@gmail.com>
2023-04-03 01:48:44 +0300

    Defined new variable of type Memory

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -5,4 +5,5 @@ import { prisma } from "../../../utils/prisma";
 
 export const put: APIRoute = async ({ request }) => {
     const { id, content } = await request.json();
+    let memoryToBeUpdated: Memory;
 }
