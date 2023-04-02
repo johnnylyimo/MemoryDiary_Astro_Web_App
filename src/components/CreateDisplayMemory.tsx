2463779118fae1bb4a0252106d26f6262bd0c89e
johnnylyimo <johnnylyimo@gmail.com>
2023-04-03 02:47:58 +0300

    Invoked  setEditMemoryPopUp method

--- src/components/CreateDisplayMemory.tsx
+++ src/components/CreateDisplayMemory.tsx
@@ -64,6 +64,7 @@ export const CreateDisplayMemory = () => {
                 })
             });
             setInputedMemory({id:'',content: '', title:''});
+            setEditMemoryPopUp(false)
         }
     }
 
